78fb42db0a110cac0d8b98941633f96350b46545
NISHANTH GOWDA CJ <nishanthjgowda9740@gmail.com>
2025-07-31 02:03:40 +0530

    Create alu_pkg.sv

---
+++ src/alu_pkg.sv
@@ -0,0 +1,10 @@
+package alu_pkg;
+  `include "alu_transaction.sv"
+  `include "alu_generator.sv"
+  `include "alu_driver.sv"
+  `include "alu_monitor.sv"
+  `include "alu_reference_model.sv"
+  `include "alu_scoreboard.sv"
+  `include "alu_environment.sv"
+  `include "alu_test.sv"
+endpackage
